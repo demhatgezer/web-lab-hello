960b3e99df75872632d1b6e1804b5280440f9b22
demhatgezer <gezerdemhat16@gmail.com>
2026-03-02 14:47:56 +0300

    feat: add personal details

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ function App() {
       <h2>LAB -1</h2>
       <p>Ad Soyad: Demhat Gezer</p>
       <p>Ogrenci No: 230541080</p>
+      <p>Bolum: Yazilim Muhendisligi</p>
+      <p>Hobi: Kod yazmak</p>
     </div>
   )
 }
