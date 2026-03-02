246064d8c019af1af35546de8be3dec517900cd7
demhatgezer <gezerdemhat16@gmail.com>
2026-03-02 14:45:53 +0300

    chore: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB -1</h2>
+      <p>Ad Soyad: Demhat Gezer</p>
+      <p>Ogrenci No: 230541080</p>
+    </div>
+  )
+}
+
+export default App
